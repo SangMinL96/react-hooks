99c14afe1520de5298a34b739fbaf9e80cdca0ef
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2020-08-05 02:07:06 +0900

    커스텀 훅,input,tep,title추가

---
+++ hooks/useTep.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const content = [
+  {
+    teb: "section1",
+    content: "섹션1111",
+  },
+  {
+    teb: "section1",
+    content: "섹션222",
+  },
+];
+
+const useTep = (initialTeb, allTeb) => {
+  if (!allTeb || !Array.isArray(allTabs)) {
+    return;
+  }
+  const [index, setIndex] = useState(initialTeb);
+
+  return {
+    currentitem: allTeb[index],
+    tepChange: setIndex,
+  };
+};
+
+// export default function App() {
+//   const { currentitem, tepChange } = useTep(0, content);
+//   return (
+//     <div className="App">
+//       {content.map((section, index) => (
+//         <button onClick={() => tepChange(index)}>{section.content}</button>
+//       ))}
+//       <div>{currentitem.content}</div>
+//     </div>
+//   );
+// }
